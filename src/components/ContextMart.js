f8594d7e96ce1d58e9044099dd9abad214c912c0
pratham930 <85028976+pratham930@users.noreply.github.com>
2021-09-13 00:29:43 +0530

    changed href to nav link used NavLink on check out page

--- src/components/ContextMart.js
+++ src/components/ContextMart.js
@@ -5,27 +5,23 @@ import { MartContext } from "./Mart";
 import { NavLink } from "react-router-dom";
 
 const ContextMart = () => {
-  const { item, totalAmount,} = useContext(MartContext);
+  const { item, totalAmount } = useContext(MartContext);
   return (
     <div>
-      
-      
       {item.map((event) => {
         return <Fast key={event.id} {...event} />;
       })}
       <div>
         <div className="downside">
-          
           <h4>
-            
             Total(INR): <span>{totalAmount}₹</span>
           </h4>
         </div>
-        <a className="check" href="/checkout">
-        save and checkout
-        </a>
+        <NavLink className="check" to="/checkout">
+          save and checkout
+        </NavLink>
 
-        <NavLink className="active_class" to='/cart'>
+        <NavLink className="active_class" to="/cart">
           cancel
         </NavLink>
       </div>
